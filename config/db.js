7d2fa989b9e31872610bd07bb5f7631d0f7e2da0
Apratim Das <apratimdas18@gmail.com>
2020-08-27 20:42:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+//Use mongoose to connect MongoDB using the Mongo URI
 const connectDb = async () => {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
